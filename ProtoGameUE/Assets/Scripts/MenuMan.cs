84665dea5ccb434ea179acd7148a277bd70abbe6
Karol Mocarski <Prottector@users.noreply.github.com>
2019-11-12 14:45:49 +0100

    Made UI, scripted some components
    -Added Coins
    -Added Lifes
    -Added Timer
    -Added points
    -Added tutorial message

---
+++ ProtoGameUE/Assets/Scripts/MenuMan.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuMan : MonoBehaviour
+{
+
+    // Use this for initialization
+    public void qGame()
+    {
+        Application.Quit();
+
+    }
+
+    public void nGame()
+    {
+        SceneManager.LoadScene("TestLvl");
+
+    }
+}
