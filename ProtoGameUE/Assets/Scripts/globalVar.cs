46bd09d3f08591930eb644a89e33bb78b0ee6ed9
Łukasz Panczykowski <lukaszpanczykowski@gmail.com>
2019-12-17 19:19:49 +0100

    Add globals vars, animation stop/start, ammo count/pickup

---
+++ ProtoGameUE/Assets/Scripts/globalVar.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class globalVar
+{
+    public static int Ammo ;
+    public static int Hp = 0;
+    public static int Score = 0;
+
+}
