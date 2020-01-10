9e6c586478b644bfb33ee6f45e703006f15c2d25
pstachow <45897848+pstachow@users.noreply.github.com>
2020-01-10 21:29:53 +0100

    coin
    coin

---
+++ ProtoGameUE/Assets/Prefabs/Collectables/Coin/CoinManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CoinManager : MonoBehaviour
+{
+    public static CoinManager instance;
+    public TextMeshProUGUI text;
+    int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(instance == null)
+        { instance = this; }
+    }
+
+    public void ChangeScore()
+    {
+        score = score + 1;
+        text.text = "SCORE: " + score.ToString();
+    }
+}
---
+++ ProtoGameUE/Assets/Prefabs/Collectables/Coin/Coin_score.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin_score : MonoBehaviour
+{
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        { CoinManager.instance.ChangeScore();
+            
+        }
+    }
+}
